060270ce82bdb7a9b78483790ddd2a45657c1733
𝗕𝗠𝗪 𝗅𝗂𝗍𝖾 <nehemiasanga654@gmail.com>
2026-06-13 22:09:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -151,9 +151,9 @@ const clientstart = async() => {
                     `> ⚡ Prefix: [ . ]\n` +
                     `> 🚀 Mode: ${sock.public ? 'Public' : 'Self'}\n` +
                     `> 🤖 Version: 1.0.0\n` +
-                    `> 👑 Owner: Debraj\n\n` +
+                    `> 👑 Owner: 𝗕𝗠𝗪 𝗅𝗂𝗍𝖾\n\n` +
                     `✅ Bot connected successfully\n` +
-                    `📢 Join our channel: https://whatsapp.com/channel/0029Va8YUl50bIdtVMYnYd0E`,
+                    `📢 Join our channel: https://whatsapp.com/channel/0029VbCjkJP2P59fBeYUuN1H`,
                 contextInfo: {
                     forwardingScore: 1,
                     isForwarded: true,
@@ -161,7 +161,7 @@ const clientstart = async() => {
                         title: config().settings.title,
                         body: config().settings.description,
                         thumbnailUrl: config().thumbUrl,
-                        sourceUrl: "https://whatsapp.com/channel/0029Va8YUl50bIdtVMYnYd0E",
+                        sourceUrl: "https://whatsapp.com/channel/0029VbCjkJP2P59fBeYUuN1H",
                         mediaType: 1,
                         renderLargerThumbnail: false
                     }
